4eef2616d7bb42d4fd14687a16e500fc2b52389e
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-22 08:56:14 -0600

    Update context.py

--- context.py
+++ context.py
@@ -3,12 +3,8 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 
 
-@dataclass
-class ClimateContext:
-
-    #
-    # Configuración
-    #
+@dataclass(slots=True)
+class ClimateConfig:
 
     unique_id: str
 
@@ -22,18 +18,41 @@ class ClimateContext:
 
     sensors: list[str] = field(default_factory=list)
 
-    #
-    # Estado
-    #
 
-    state: str | None = None
+@dataclass(slots=True)
+class ClimateState:
+
+    available: bool = False
+
+    is_on: bool = False
+
+    hvac_mode: str | None = None
+
+    hvac_action: str | None = None
 
     current_temperature: float | None = None
 
     target_temperature: float | None = None
 
-    hvac_mode: str | None = None
+    target_temperature_high: float | None = None
+
+    target_temperature_low: float | None = None
 
     fan_mode: str | None = None
 
     swing_mode: str | None = None
+
+    preset_mode: str | None = None
+
+    humidity: int | None = None
+
+    target_humidity: int | None = None
+
+
+class ClimateContext:
+
+    def __init__(self, config: ClimateConfig):
+
+        self.config = config
+
+        self.state = ClimateState()
