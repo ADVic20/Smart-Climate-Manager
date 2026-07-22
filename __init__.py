08da429603f57d5b4a8d4d7d51e38501aaaf0d2a
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-22 09:00:25 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,21 +4,28 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
-self.service = ClimateService(hass)
 
-PLATFORMS = []
+PLATFORMS: list[str] = [
+    "climate",
+]
 
 
-async def async_setup(hass: HomeAssistant, config: dict):
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    """Set up Smart Climate Manager."""
     return True
 
 
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-):
+) -> bool:
+    """Set up Smart Climate Manager from a config entry."""
+
     hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.data[DOMAIN][entry.entry_id] = {}
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
@@ -31,7 +38,18 @@ async def async_setup_entry(
 async def async_unload_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
-):
-    hass.data[DOMAIN].pop(entry.entry_id)
+) -> bool:
+    """Unload a config entry."""
 
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
+
+    if unload_ok:
+        hass.data[DOMAIN].pop(
+            entry.entry_id,
+            None,
+        )
+
+    return unload_ok
