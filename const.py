ec89b86cc42b50e6747a4bc3be8c96c768f1ee6b
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-22 08:30:20 -0600

    Add constants for climate manager configuration

---
+++ const.py
@@ -0,0 +1,7 @@
+DOMAIN = "smart_climate_manager"
+
+CONF_NAME = "name"
+CONF_ALLOW_FORCE = "allow_force"
+CONF_VIRTUAL_CLIMATE = "virtual_climate"
+CONF_REAL_CLIMATE = "real_climate"
+CONF_BLOCK_SENSORS = "block_sensors"
